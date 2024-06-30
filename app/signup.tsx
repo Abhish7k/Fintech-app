2aad118d598268b43fc5340b3a4cffe9e47fbdaf
Abhishek <abhishekbhosale.dev@gmail.com>
2024-07-01 01:00:41 +0530

    feat: sign up screen
    built the basic ui of sign up screen

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,68 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const signup = () => {
+  const [countryCode, setCountryCode] = useState("+91");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const onSignup = async () => {};
+
   return (
-    <View>
-      <Text>signup</Text>
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.header}>Let's Get Started !</Text>
+      <Text style={defaultStyles.descriptionText}>
+        Please Enter your phone number. We will send you a confirmation code
+        over there
+      </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.countryCodeInput}
+          placeholder="Enter your phone number"
+          placeholderTextColor={Colors.gray}
+          value={countryCode}
+        />
+        <TextInput
+          style={styles.mobileNumberInput}
+          placeholder="Mobile number"
+          placeholderTextColor={Colors.gray}
+          keyboardType="numeric"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  countryCodeInput: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+    maxWidth: 80,
+  },
+  mobileNumberInput: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+    width: "auto",
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
 
 export default signup;
