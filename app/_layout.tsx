9236e221ba888bd68aba3c4f841a583bdb5c4517
Abhishek <abhishekbhosale.dev@gmail.com>
2024-07-24 16:59:38 +0530

    feat: layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
+import Colors from "@/constants/Colors";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
 export {
@@ -37,10 +39,35 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
-function RootLayoutNav() {
+const InitialLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="signup"
+        options={{
+          title: "Sign Up",
+          headerBackTitle: "",
+          headerStyle: { backgroundColor: Colors.background },
+        }}
+      />
+
+      <Stack.Screen
+        name="login"
+        options={{
+          title: "Login",
+          headerBackTitle: "",
+          headerStyle: { backgroundColor: Colors.background },
+        }}
+      />
     </Stack>
+  );
+};
+
+function RootLayoutNav() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <InitialLayout />
+    </GestureHandlerRootView>
   );
 }
